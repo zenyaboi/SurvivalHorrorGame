c5e0eff1fd93514492e4393de5de17d6ff91c274
Gabriel Franke <varne0918@gmail.com>
2025-07-14 23:06:52 -0300

    added funcionality for showing interact icon

--- Source/SurvivalHorrorGame/Items/BaseItem.cpp
+++ Source/SurvivalHorrorGame/Items/BaseItem.cpp
@@ -1,16 +1,58 @@
 #include "SurvivalHorrorGame/Items/BaseItem.h"
+#include "Components/SphereComponent.h"
+#include "Components/BillboardComponent.h"
 
 // Sets default values
 ABaseItem::ABaseItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	isInnerOverlapped = false;
+	isOuterOverlapped = false;
 }
 
 // Called when the game starts or when spawned
 void ABaseItem::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	TArray<USphereComponent*> Spheres;
+	GetComponents<USphereComponent>(Spheres);
+	
+	for (USphereComponent* Sphere : Spheres)
+	{
+		if (Sphere->GetName().Contains(TEXT("inner")))
+			InnerSphere = Sphere;
+		else if (Sphere->GetName().Contains(TEXT("outer")))
+			OuterSphere = Sphere;
+	}
+	
+	TArray<UBillboardComponent*> Icons;
+	GetComponents<UBillboardComponent>(Icons);
+	
+	for (UBillboardComponent* Icon : Icons)
+	{
+		if (Icon->GetName().Contains(TEXT("inner")))
+			InnerIcon = Icon;
+		else if (Icon->GetName().Contains(TEXT("outer")))
+			OuterIcon = Icon;
+	}
+	
+	if (InnerIcon) InnerIcon->SetVisibility(false);
+	if (OuterIcon) OuterIcon->SetVisibility(false);
+	
+	if (InnerSphere)
+	{
+		InnerSphere->OnComponentBeginOverlap.AddDynamic(this, &ABaseItem::OnInnerBeginOverlap);
+		InnerSphere->OnComponentEndOverlap.AddDynamic(this, &ABaseItem::OnInnerEndOverlap);
+	}
+	
+	if (OuterSphere)
+	{
+		OuterSphere->OnComponentBeginOverlap.AddDynamic(this, &ABaseItem::OnOuterBeginOverlap);
+		OuterSphere->OnComponentEndOverlap.AddDynamic(this, &ABaseItem::OnOuterEndOverlap);
+	}
 }
 
 // Called every frame
@@ -19,3 +61,52 @@ void ABaseItem::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+void ABaseItem::OnInnerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Inner Sphere Begin Overlap with: %s"), *OtherActor->GetName());
+
+	isInnerOverlapped = true;
+	
+	if (InnerIcon) 
+	{
+		InnerIcon->SetVisibility(true);
+		UE_LOG(LogTemp, Warning, TEXT("Inner Icon mostrado"));
+	}
+}
+
+void ABaseItem::OnInnerEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Inner Sphere End Overlap with: %s"), *OtherActor->GetName());
+
+	isInnerOverlapped = false;
+	
+	if (InnerIcon) 
+	{
+		InnerIcon->SetVisibility(false);
+		UE_LOG(LogTemp, Warning, TEXT("Inner Icon escondido"));
+	}
+}
+
+void ABaseItem::OnOuterBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Outer Sphere Begin Overlap with: %s"), *OtherActor->GetName());
+	
+	if (OuterIcon) 
+	{
+		if (isInnerOverlapped)
+			OuterIcon->SetVisibility(false);
+		OuterIcon->SetVisibility(true);
+		UE_LOG(LogTemp, Warning, TEXT("Outer Icon mostrado"));
+	}
+}
+
+void ABaseItem::OnOuterEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Outer Sphere End Overlap with: %s"), *OtherActor->GetName());
+	
+	if (OuterIcon) 
+	{
+		OuterIcon->SetVisibility(false);
+		UE_LOG(LogTemp, Warning, TEXT("Outer Icon escondido"));
+	}
+}
--- Source/SurvivalHorrorGame/Items/BaseItem.h
+++ Source/SurvivalHorrorGame/Items/BaseItem.h
@@ -17,6 +17,33 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY()
+	class USphereComponent* InnerSphere;
+	
+	UPROPERTY()
+	class USphereComponent* OuterSphere;
+	
+	UPROPERTY()
+	class UBillboardComponent* InnerIcon;
+	
+	UPROPERTY()
+	class UBillboardComponent* OuterIcon;
+
+	UFUNCTION()
+	void OnInnerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnInnerEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	UFUNCTION()
+	void OnOuterBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnOuterEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	bool isInnerOverlapped;
+	bool isOuterOverlapped;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
