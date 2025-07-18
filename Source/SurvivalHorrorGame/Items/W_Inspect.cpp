c2b594bf0a6b000b194ddc52b04f4c1baddf7710
Gabriel Franke <varne0918@gmail.com>
2025-07-17 23:40:44 -0300

    created inspect bp widget and script widget

---
+++ Source/SurvivalHorrorGame/Items/W_Inspect.cpp
@@ -0,0 +1,1 @@
+#include "W_Inspect.h"
---
+++ Source/SurvivalHorrorGame/Items/W_Inspect.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "W_Inspect.generated.h"
+
+UCLASS()
+class SURVIVALHORRORGAME_API UW_Inspect : public UUserWidget
+{
+	GENERATED_BODY()
+};
