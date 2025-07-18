294b0f6b1d6dbf5bd650c743be2d52b829c57a7d
Gabriel Franke <varne0918@gmail.com>
2025-07-18 14:28:10 -0300

    created inspect ui

--- Source/SurvivalHorrorGame/Items/W_Inspect.h
+++ Source/SurvivalHorrorGame/Items/W_Inspect.h
@@ -2,10 +2,17 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
 #include "W_Inspect.generated.h"
 
 UCLASS()
 class SURVIVALHORRORGAME_API UW_Inspect : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* ItemName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* ItemDescription;
 };
